e0bec30540382e1e994ed7ec7198f0ffd2fc77f3
Thomas Schalldach <tom@riddle.wtf>
2023-06-16 13:24:57 +0200

    allowed math engine delay

--- async-request-reply/math-engine/src/main/java/com/schalldach/mathengine/data/Action.java
+++ async-request-reply/math-engine/src/main/java/com/schalldach/mathengine/data/Action.java
@@ -1,7 +1,29 @@
 package com.schalldach.mathengine.data;
 
-public enum Action {
-    ADD, SUBTRACT, MULTIPLY, DIVIDE;
+import com.schalldach.mathengine.operation.MathematicalOperation;
+
+public enum Action implements MathematicalOperation {
+    ADD{
+        @Override
+        public int calculate(int left, int right) {
+            return left+right;
+        }
+    }, SUBTRACT {
+        @Override
+        public int calculate(int left, int right) {
+            return left-right;
+        }
+    }, MULTIPLY {
+        @Override
+        public int calculate(int left, int right) {
+            return left*right;
+        }
+    }, DIVIDE {
+        @Override
+        public int calculate(int left, int right) {
+            return left/right;
+        }
+    };
 
 
 }
